eb0fa1cb7fa438966e3c2d00638506f694d35c24
rithesh singh <78195812+rikkx@users.noreply.github.com>
2021-03-01 23:31:57 +0530

    Create switch_menu.c

---
+++ switch_menu.c
@@ -0,0 +1,47 @@
+//1 march 2021
+//Rithesh singh
+//switcn case: menu driven program
+#include <stdio.h>
+#include <math.h>
+int main() {
+int value, area;
+scanf("%d",&value);
+switch(value) 
+{
+    case 1:
+    printf("its a square");
+    int side ;
+    scanf("%d",&side);
+    area = side * side;
+    printf("\narea of the given square is \t%d",area);
+    break;
+
+    case 2:
+    printf("its a rectangle ");
+    int length, breadth ;
+    scanf("%d %d",&length, &breadth);
+    area = length* breadth;
+    printf("\narea of the given square is \t%d",area);
+    break;
+
+    case 3:
+    printf("its a triangle ");
+    int base , height  ;
+    scanf("%d %d",&base, &height);
+    area = base* height;
+    printf("\narea of the given square is \t%d",area);
+    break;
+
+    case 4:
+    printf("its a circle");
+    int radius ;
+    scanf("%d",&radius);
+    float area = 3.14 * radius * radius;
+    printf("\narea of the given square is \t%f",area);
+    break;
+
+    default :
+    printf("invalid entry");
+}
+return 0;
+}
